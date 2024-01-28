97bf2ab6e2776a1b4a6eb41809378a74472df656
NedStalker <153412408+NedStalker@users.noreply.github.com>
2024-01-28 15:31:37 +0500

    Add files via upload

---
+++ Cheats.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Game.h"
+#include "Entity.h"
+#include "AimBot.hpp"
+#include "Radar/Radar.h"
+#include "TriggerBot.h"
+
+#include "Features/Glow.h"
+#include "Features/Misc.h"
+
+namespace Cheats
+{
+	void KeyCheckThread();
+
+	void RenderCrossHair(ImDrawList* drawList) noexcept;
+
+	void RadarSetting(Base_Radar& Radar);
+	void Run();
+}
+
+struct {
+	ImFont* normal15px = nullptr;
+} fonts;
